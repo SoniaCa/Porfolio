90b344f35f98f125491fc688ce698603d6e37dca
SoniaCa <carmon.sonia@gmail.com>
2021-04-17 22:35:50 +0200

    footer favicon nav contact

--- portfolio/src/Controller/MainController.php
+++ portfolio/src/Controller/MainController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +14,7 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function home(): Response
     {
         return $this->render('main/home.html.twig', [
             'controller_name' => 'HomeController',
@@ -51,10 +54,24 @@ class MainController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(): Response
+    public function contact(Request $request): Response
     {
+       
+       $contact = new Contact();
+       $form = $this->createForm(ContactType::class, $contact);
+       $form->handleRequest($request);
+       
+       if ($form->isSubmitted() && $form->isValid()) {
+           $em = $this->getDoctrine()->getManager();
+           $em->persist($contact);
+           $em->flush();
+
+           $this->addFlash('success', 'Votre message a bien été envoyé !');
+           return $this->redirectToRoute('contact');
+       }
+
         return $this->render('main/contact.html.twig', [
-            'controller_name' => 'HomeController',
+            'form' => $form->createView(),
         ]);
     }
 }
