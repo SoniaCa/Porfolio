3e7d4ffa38efe7b55ee577dfe0a58d8b21d01b4f
SoniaCa <carmon.sonia@gmail.com>
2021-05-02 18:27:55 +0200

    peaufinage de la homepage de détail, ajout page projet

--- portfolio/src/Controller/MainController.php
+++ portfolio/src/Controller/MainController.php
@@ -5,9 +5,12 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class MainController extends AbstractController
 {
@@ -19,6 +22,28 @@ class MainController extends AbstractController
         return $this->render('main/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    /**
+    * @Route("/cv", name="cv")
+    */
+    public function cv()
+    {
+        // send the file contents and force the browser to download it
+        return $this->file('assets/CV.pdf', 'CVSoniaCarmon.pdf', ResponseHeaderBag::DISPOSITION_INLINE);
+        //Chemin à partir du dossier public 
+            
+    }
+
+     /**
+    * @Route("/prog", name="prog")
+    */
+    public function prog()
+    {
+        // send the file contents and force the browser to download it
+        return $this->file('assets/prog.pdf', 'ProgrammeOclock.pdf', ResponseHeaderBag::DISPOSITION_INLINE);
+        //Chemin à partir du dossier public 
+            
     }
 
     /**
@@ -42,11 +67,22 @@ class MainController extends AbstractController
     }
 
     /**
-     * @Route("/experience", name="exp")
+     * @Route("/experiences", name="exp")
      */
     public function exp(): Response
     {
         return $this->render('main/exp.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/projects", name="projects")
+     */
+    public function projects(Request $request): Response
+    {
+       
+        return $this->render('main/projects.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
--- portfolio/src/Form/ContactType.php
+++ portfolio/src/Form/ContactType.php
@@ -4,17 +4,39 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('name')
-            ->add('message')
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'label' => 'Email ',
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+            ->add('name', TextType::class, [
+                'required' => true,
+                'label' => 'Nom ',
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
+            ->add('message', TextareaType::class, [
+                'required' => true,
+                'label' => 'Votre message ',
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ])
         ;
     }
 
